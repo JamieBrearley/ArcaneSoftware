0e1bd4c74414e131ac0276accda70b6f93b4bd14
JamieBrearley <jamie.brearley@arcanesoftware.com.au>
2021-08-10 18:22:46 +1000

    Error Pages

---
+++ error-403.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once('inc/head.php'); ?>
+    <title>Arcane Software - Error 403</title>
+
+</head>
+
+<body style="opacity: 0;">
+    <?php require_once('inc/header.php'); ?>
+    <main>
+        <?php require('inc/hamburgerBtn.php'); ?>
+        <section>
+            <h1>Error 403</h1>
+            <p>Page forbidden.</p>
+        </section>
+    </main>
+    <?php require_once('inc/footer.php'); ?>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
---
+++ error-404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once('inc/head.php'); ?>
+    <title>Arcane Software - Error 404</title>
+
+</head>
+
+<body style="opacity: 0;">
+    <?php require_once('inc/header.php'); ?>
+    <main>
+        <?php require('inc/hamburgerBtn.php'); ?>
+        <section>
+            <h1>Error 404</h1>
+            <p>Page not found.</p>
+        </section>
+    </main>
+    <?php require_once('inc/footer.php'); ?>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
