2fb7173f92d099878d50f68998e08639b73bad22
JamieBrearley <jamie.brearley@arcanesoftware.com.au>
2021-08-10 22:47:23 +1000

    no more relative links in error pages.

--- error/404.php
+++ error/404.php
@@ -2,22 +2,61 @@
 <html lang="en">
 
 <head>
-    <?php require_once('../inc/head.php'); ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS stylesheets -->
+    <link rel="stylesheet" href="https://www.arcanesoftware.com.au/css/global.css">
+    <link rel="stylesheet" href="https://www.arcanesoftware.com.au/css/header.css">
+    <link rel="stylesheet" href="https://www.arcanesoftware.com.au/css/main.css">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="https://www.arcanesoftware.com.au/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://www.arcanesoftware.com.au/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://www.arcanesoftware.com.au/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="https://www.arcanesoftware.com.au/assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="https://www.arcanesoftware.com.au/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3G4YBL3W22"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-3G4YBL3W22');
+    </script>
+
     <title>Arcane Software - Error 404</title>
     <meta name="robots" content="noindex">
 </head>
 
 <body style="opacity: 0;">
-    <?php require_once('../inc/header.php'); ?>
+    <?php require_once('https://www.arcanesoftware.com.au/inc/header.php'); ?>
     <main>
-        <?php require('../inc/hamburgerBtn.php'); ?>
+        <?php require('https://www.arcanesoftware.com.au/inc/hamburgerBtn.php'); ?>
         <section>
             <h1><span class="fancy">Error 404:</span> Page not found.</h1>
             <p>The goblins ran away with the damn page again..</p>
+            <a href="https://arcanesoftware.com.au" title="home page">Return to Homepage</a>
         </section>
     </main>
-    <?php require_once('../inc/footer.php'); ?>
-    <script src="js/index.js"></script>
+    <footer></footer>
+
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/57ed6e10c3.js" crossorigin="anonymous"></script>
+
+    <script src="https://www.arcanesoftware.com.au/js/global.js"></script>
 </body>
 
 </html>
