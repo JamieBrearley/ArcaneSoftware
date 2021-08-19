d2e227ffe3da3e4b7c651fdea1c7e1515c7c6e33
JamieBrearley <jamie.brearley@arcanesoftware.com.au>
2021-08-19 22:12:36 +1000

    website photography

--- websolutions/website_photography.php
+++ websolutions/website_photography.php
@@ -3,9 +3,16 @@
 
 <head>
     <?php require_once('../inc/head.php'); ?>
-    <title>Arcane Software | Website Photography</title>
+    <title>Arcane Software | Website Photography Brisbane</title>
 
-    <meta name="description" content="Arcane Software is a full-service digital marketing agency devoted to developing skills that help you outperform your competition. We use a combination of data analysis, technical intellect, and web expertise to make intelligent, informed decisions that help your business expand and grow online, from Search Engine Optimisation (SEO) to Social Media management and eye-catching web design.">
+    <meta name="description" content="Arcane Software is a web design company based in Brisbane. They provide quality website photography services to small businesses, startups & entrepreneurs at an affordable price. Contact us here on how you can benefit from our services! Get free quote today!" />
+
+    <!-- Open Graph Markup -->
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Website Photography" />
+    <meta property="og:description" content="Arcane Software is a web design company based in Brisbane. They provide quality website photography services to small businesses, startups & entrepreneurs at an affordable price. Contact us here on how you can benefit from our services! Get free quote today!" />
+    <meta property="og:image" content="https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-512.jpg" />
+    <meta property="og:image:secure_url" content="https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-512.jpg" />
 </head>
 
 <body style="opacity: 0;">
@@ -13,13 +20,28 @@
     <main>
         <?php require('../inc/hamburgerBtn.php'); ?>
         <section>
-            <h1>Coming Soon!</h1>
-            <p>This page is under development and will be here soon!</p>
+            <h1>Personalised <span class="fancy">Photography</span> for your website</h1>
+            <p>The first impression is a killer when it comes to marketing. You want your call-to-action button and image to be the best of both worlds, but striking that perfect balance can make for an exhausting experience.</p>
+            <p>When you're trying to create the right combination with your website's design, going through all possible options may seem exhaustively time consuming at times - especially if they aren't getting the expected results! Finding this specific combo is done one step at a time, by having strong visuals alongside great copywriting skills.</p>
+            <a class="btn-animate" href="../contact_us.php">Book a Photographer</a>
+        </section>
+        <img id="pageImg" src="https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-512.jpg" alt="Arcane Software website photography" sizes="(max-width: 700px) 100vw, 70vw" srcset="https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-1024.jpg 1024w, https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-512.jpg 512w, https://arcane-software-bucket.s3.ap-southeast-2.amazonaws.com/images/website-photography-256.jpg 256w">
+        <section>
+            <h2>Photography Services</h2>
+            <p>Photos speak a thousand words and we know that your online presence is vital with so many competitors. That's why at Arcane Software, our web design team creates images to tell the story of your business in an engaging way!</p>
+            <p>For better web photography, our Brisbane-based photographers need to work closely with you and the environment. Our team will craft an overall vision for your company that incorporates all of its key aspects.</p>
+        </section>
+        <section>
+            <h2><span class="fancy">Stock</span> Photography</h2>
+            <p>We've scoured picture websites for years to find the perfect combination of photos that create a compelling story through imagery. We know how important authenticity is and urge companies not to fall into generic or boring pictures which will do nothing but waste your budget. If you want us to help make your brand stand out from others, we have plenty of experience finding just what it takes!</p>
+        </section>
+        <section>
+            <h2>Photo Editing</h2>
+            <p>Arcane Software are able to do high-quality photo editing, retouching, and more. Our experienced staff, are dedicated to providing you with the highest quality service possible. We work with a wide range of photo types, including landscape, portrait, event, wedding, and product photography to help your website stand out among its competitors.</p>
+            <a class="btn-animate" href="../contact_us.php">Enquire now!</a>
         </section>
     </main>
-
     <?php require_once('../inc/footer.php'); ?>
-
 </body>
 
 </html>
